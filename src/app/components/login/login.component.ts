576e34bf24dcc4cae71bdda7396cf347043db929
Sascha Nemeth <sascha.nemeth@me.com>
2024-08-10 12:15:58 +0200

    chore: Add header component to login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
