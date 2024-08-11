67865e8d2192ed31dcaca963b1fbfbd8c6c2c327
Sascha Nemeth <sascha.nemeth@me.com>
2024-08-11 10:46:25 +0200

    chore: Update home component markup and styles

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [HeaderComponent, FooterComponent],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: "app-login",
+    standalone: true,
+    imports: [HeaderComponent, FooterComponent],
+    templateUrl: "./login.component.html",
+    styleUrl: "./login.component.scss",
 })
 export class LoginComponent {
- isCheckboxChecked = false;
+     isCheckboxChecked = false;
 
- toggleSubmitButton(event: Event) {
-   this.isCheckboxChecked = (event.target as HTMLInputElement).checked;
- }
+     toggleSubmitButton(event: Event) {
+       this.isCheckboxChecked = (event.target as HTMLInputElement).checked;
+     }
+
+    email: string = "";
+
+    constructor(private route: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        this.route.queryParams.subscribe((params) => {
+            this.email = params["email"] || "";
+        });
+    }
 }
