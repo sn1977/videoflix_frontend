d8d5fe060b995dfa3f86c000c0223b2a5bfb9127
Sascha Nemeth <sascha.nemeth@me.com>
2024-08-11 12:07:38 +0200

    chore: Update header component markup and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistryComponent } from './components/registry/registry.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegistryComponent}
 ];
---
+++ src/app/components/registry/registry.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registry',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, FormsModule],
+  templateUrl: './registry.component.html',
+  styleUrl: './registry.component.scss'
+})
+export class RegistryComponent {
+email: string = "";
+
+}
