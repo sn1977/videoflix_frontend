2a8c98034084cc6a50976b1ddea4493f8dd322d3
Sascha Nemeth <sascha.nemeth@me.com>
2024-08-11 12:49:11 +0200

    chore: Update registry component to validate password equality before submitting the form

--- src/app/components/registry/registry.component.ts
+++ src/app/components/registry/registry.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: "app-registry",
     standalone: true,
-    imports: [HeaderComponent, FooterComponent, FormsModule],
+    imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule],
     templateUrl: "./registry.component.html",
     styleUrl: "./registry.component.scss",
 })
@@ -14,8 +15,17 @@ export class RegistryComponent {
     email: string = "";
     password: string = "";
     confirmPassword: string = "";
+    // passwordsEqual: boolean = false;
+
+    get passwordsEqual(): boolean {
+      return this.password === this.confirmPassword;
+    }
 
     onSubmit() {
+      if (this.passwordsEqual) {
         console.log(this.email, this.password, this.confirmPassword);
+      } else {
+        console.log("passwords not equal");
+      }
     }
 }
