bd72d75ea392ca91223d22e8c03e2764bbeb1686
Sascha Nemeth <sascha.nemeth@me.com>
2024-08-31 13:41:16 +0200

    Update footer component to use translated links for data privacy and imprint

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from "../../services/language.service";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  currentLanguage: string;
+
+  constructor(private languageService: LanguageService
+  ) {
+      this.currentLanguage = this.languageService.getCurrentLanguage();
+  }
 
 }
