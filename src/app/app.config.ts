1aed04fbfb91ee044c0b32345a9b1dd3c76fb041
Sascha Nemeth <sascha.nemeth@me.com>
2024-09-01 14:34:02 +0200

    chore: Update package.json to include Sentry sourcemaps upload script

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,8 @@ import {
     ApplicationConfig,
     importProvidersFrom,
     provideZoneChangeDetection,
+    ErrorHandler,
+    APP_INITIALIZER,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideClientHydration } from "@angular/platform-browser";
@@ -15,6 +17,7 @@ import {
     TranslateModule,
 } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import * as Sentry from "@sentry/angular";
 
 import { routes } from "./app.routes";
 
@@ -22,26 +25,63 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     return new TranslateHttpLoader(http, "/assets/i18n/", ".json");
 }
 
+// export const appConfig: ApplicationConfig = {
+//     providers: [
+//         provideZoneChangeDetection({ eventCoalescing: true }),
+//         provideRouter(routes),
+//         provideClientHydration(),
+//         importProvidersFrom(HttpClientModule),
+//         importProvidersFrom(
+//             TranslateModule.forRoot({
+//                 loader: {
+//                     provide: TranslateLoader,
+//                     useFactory: HttpLoaderFactory,
+//                     deps: [HttpClient],
+//                 },
+//                 compiler: {
+//                     provide: TranslateCompiler,
+//                     useClass: TranslateFakeCompiler,
+//                 },
+//             })
+//         ),
+//         TranslateService,
+//         TranslateStore,
+//     ],
+// };
+
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
-        provideClientHydration(),
-        importProvidersFrom(HttpClientModule),
-        importProvidersFrom(
-            TranslateModule.forRoot({
-                loader: {
-                    provide: TranslateLoader,
-                    useFactory: HttpLoaderFactory,
-                    deps: [HttpClient],
-                },
-                compiler: {
-                    provide: TranslateCompiler,
-                    useClass: TranslateFakeCompiler,
-                },
-            })
-        ),
-        TranslateService,
-        TranslateStore,
-    ],
+  providers: [
+      provideZoneChangeDetection({ eventCoalescing: true }),
+      provideRouter(routes),
+      provideClientHydration(),
+      importProvidersFrom(HttpClientModule),
+      importProvidersFrom(
+          TranslateModule.forRoot({
+              loader: {
+                  provide: TranslateLoader,
+                  useFactory: HttpLoaderFactory,
+                  deps: [HttpClient],
+              },
+              compiler: {
+                  provide: TranslateCompiler,
+                  useClass: TranslateFakeCompiler,
+              },
+          })
+      ),
+      TranslateService,
+      TranslateStore,
+      {
+          provide: ErrorHandler,
+          useValue: Sentry.createErrorHandler({
+              showDialog: true,  // Optional: Zeigt bei Fehlern einen Dialog an
+          }),
+      },
+      Sentry.TraceService,
+      {
+          provide: APP_INITIALIZER,
+          useFactory: () => () => {},
+          deps: [Sentry.TraceService],
+          multi: true,
+      },
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,30 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// import { enableProdMode } from "@angular/core";
+// import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import * as Sentry from "@sentry/angular";
+
+// import { AppModule } from "./app/app.module";
+
 bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers
   ]
 })
 .catch(err => console.error(err));
+
+Sentry.init({
+  dsn: "https://44b4d21598085109e9d8ef49ee94bc35@o4507877077024768.ingest.de.sentry.io/4507877207441488",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
