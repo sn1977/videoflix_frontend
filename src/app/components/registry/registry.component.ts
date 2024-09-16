17ef35e3bfc3d1be8c530cc20fbff7f9ea95da50
Sascha Nemeth <sascha.nemeth@me.com>
2024-09-16 08:45:22 +0200

    Refactor registry component to add username input field and registration functionality

--- src/app/components/registry/registry.component.ts
+++ src/app/components/registry/registry.component.ts
@@ -5,11 +5,19 @@ import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { TranslateModule } from "@ngx-translate/core";
 import { LanguageService } from "../../services/language.service";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "app-registry",
     standalone: true,
-    imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule, TranslateModule],
+    imports: [
+        HeaderComponent,
+        FooterComponent,
+        FormsModule,
+        CommonModule,
+        TranslateModule,
+    ],
     templateUrl: "./registry.component.html",
     styleUrl: "./registry.component.scss",
 })
@@ -21,20 +29,36 @@ export class RegistryComponent {
     // passwordsEqual: boolean = false;
     currentLanguage: string;
 
-    constructor(private languageService: LanguageService
+    constructor(
+        private languageService: LanguageService,
+        private authService: AuthService,
+        private router: Router
     ) {
         this.currentLanguage = this.languageService.getCurrentLanguage();
     }
 
     get passwordsEqual(): boolean {
-      return this.password === this.confirmPassword;
+        return this.password === this.confirmPassword;
     }
 
     onSubmit() {
-      if (this.passwordsEqual) {
-        console.log(this.email, this.password, this.confirmPassword);
-      } else {
-        console.log("passwords not equal");
-      }
+        if (this.passwordsEqual) {
+            console.log(this.email, this.password, this.confirmPassword);
+        } else {
+            console.log("passwords not equal");
+        }
+        try {
+            const resp = this.authService.register(
+                this.username,
+                this.email,
+                this.password
+            );
+            console.log("Response:", resp);
+            alert("Registration successful");
+            this.router.navigate(["/login/"]);
+        } catch (error) {
+            console.error("Error:", error);
+            alert("Registration failed");
+        }
     }
 }
