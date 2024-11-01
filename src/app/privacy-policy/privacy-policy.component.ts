4068ae4ce675ae34558fe60d2fe6b78851ab7471
Sascha Nemeth <sascha.nemeth@me.com>
2024-11-01 11:16:02 +0100

    Refactor component styles to update background and height properties

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,19 +1,25 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateModule} from '@ngx-translate/core';
 import { LanguageService } from "../services/language.service";
-
+import { FooterComponent } from "../components/footer/footer.component";
+import { HeaderComponent } from "../components/header/header.component";
+import { Location } from "@angular/common";
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, FooterComponent, HeaderComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent implements OnInit{
-    constructor(private languageService: LanguageService) {
+    constructor(private languageService: LanguageService, private location: Location) {
     }
-    
+
     ngOnInit(): void {
         window.scrollTo(0, 0);
     }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
