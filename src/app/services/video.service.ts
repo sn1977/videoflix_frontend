c84ba7a4988712cff33bb530e581f3d4e45c3dc1
Sascha Nemeth <sascha.nemeth@me.com>
2024-11-03 19:19:30 +0100

    Refactor video.service.ts to improve code formatting and add method documentation

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -1,46 +1,3 @@
-// import { HttpClient, HttpHeaders } from "@angular/common/http";
-// import { Injectable } from "@angular/core";
-// import { environment } from "../../environments/environment";
-// import { lastValueFrom, Observable, throwError } from "rxjs";
-// import { Video } from "../models/video-model";
-
-// @Injectable({
-//     providedIn: "root",
-// })
-// export class VideoService {
-//     constructor(private http: HttpClient) {}
-
-//     private getHeaders(): HttpHeaders {
-//       const token = localStorage.getItem('token');
-//       return new HttpHeaders({
-//         'Content-Type': 'application/json',
-//         'Authorization': `Token ${token}`
-//       });
-//     }
-
-//     // getVideos() {
-//     //     const url = environment.apiUrl + "/video_selection/";
-//     //     return lastValueFrom(this.http.get(url, { headers: this.getHeaders() }));
-//     // }
-
-//     getVideos(): Observable<Video[]> {
-//       if (typeof window !== 'undefined' && localStorage.getItem('token')) {
-//           // const token = localStorage.getItem('token');
-//           // const headers = new HttpHeaders({
-//           //     'Authorization': `Token ${token}`
-//           // });
-
-//           const url = environment.apiUrl + "/video_selection/";
-//           return this.http.get<Video[]>(url, { headers: this.getHeaders() });
-//       } else {
-//           console.error("localStorage is not available or token is missing");
-//           return throwError(() => new Error('localStorage is not available or token is missing'));
-//       }
-//   }
-// }
-
-// src/app/services/video.service.ts
-
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "../../environments/environment";
@@ -61,6 +18,16 @@ export class VideoService {
         });
     }
 
+    /**
+     * Fetches a list of videos from the server.
+     * 
+     * This method checks if the `window` object is defined and if a token is present in `localStorage`.
+     * If both conditions are met, it constructs the API URL and makes an HTTP GET request to fetch the videos.
+     * If either condition is not met, it logs an error message and returns an observable that throws an error.
+     * 
+     * @returns {Observable<Video[]>} An observable that emits an array of `Video` objects.
+     * @throws {Error} If `localStorage` is not available or the token is missing.
+     */
     getVideos(): Observable<Video[]> {
         if (typeof window !== "undefined" && localStorage.getItem("token")) {
             const url = environment.apiUrl + "/video_selection/";
@@ -76,8 +43,13 @@ export class VideoService {
         }
     }
 
+    /**
+     * Fetches the list of categories from the API.
+     *
+     * @returns {Observable<Category[]>} An observable that emits the list of categories.
+     * @throws Will throw an error if fetching categories fails or if localStorage is not available or the token is missing.
+     */
     getCategories(): Observable<Category[]> {
-        // Aktualisiert
         if (typeof window !== "undefined" && localStorage.getItem("token")) {
             const url = environment.apiUrl + "/categories/";
             return this.http
@@ -101,8 +73,16 @@ export class VideoService {
         }
     }
 
+    /**
+     * Fetches a video by its ID.
+     * 
+     * @param id - The ID of the video to fetch.
+     * @returns An Observable that emits the fetched Video object.
+     * 
+     * @throws Will throw an error if localStorage is not available or the token is missing.
+     * @throws Will throw an error if there is an issue with the HTTP request.
+     */
     getVideoById(id: number): Observable<Video> {
-        // Neue Methode
         if (typeof window !== "undefined" && localStorage.getItem("token")) {
             const url = `${environment.apiUrl}/videos/${id}/`;
             return this.http
