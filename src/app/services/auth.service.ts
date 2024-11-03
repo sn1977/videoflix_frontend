98522172f0d785ecf09383ba66173781c3dc017d
Sascha Nemeth <sascha.nemeth@me.com>
2024-11-03 19:16:28 +0100

    Refactor auth.service.ts to improve code formatting and add method documentation

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,11 +11,17 @@ export class AuthService {
 
     constructor(private http: HttpClient) {}
 
+    /**
+     * Logs in a user using either their email or username and password.
+     *
+     * @param emailOrUsername - The user's email or username.
+     * @param password - The user's password.
+     * @returns A promise that resolves with the server response.
+     */
     public loginWithEmailOrUsernameAndPassword(
         emailOrUsername: string,
         password: string
     ) {
-        // const url = environment.apiUrl + '/login/';
         const url = `${environment.apiUrl}/login/`; // Verwenden Sie Template-Strings für Klarheit
         const body = {
             email_or_username: emailOrUsername,
@@ -24,6 +30,16 @@ export class AuthService {
         return lastValueFrom(this.http.post(url, body));
     }
 
+    /**
+     * Registers a new user with the provided details.
+     *
+     * @param firstname - The first name of the user.
+     * @param lastname - The last name of the user.
+     * @param username - The username for the new account.
+     * @param email - The email address of the user.
+     * @param password - The password for the new account.
+     * @returns A promise that resolves when the registration request is complete.
+     */
     public register(
         firstname: string,
         lastname: string,
@@ -31,7 +47,6 @@ export class AuthService {
         email: string,
         password: string
     ) {
-        // const url = environment.apiUrl + "/register/";
         const url = `${environment.apiUrl}/register/`;
         const body = {
             first_name: firstname,
@@ -40,30 +55,55 @@ export class AuthService {
             email: email,
             password: password,
         };
-        // return lastValueFrom(this.http.post(url, body));
         return lastValueFrom(
             this.http.post(url, body, { withCredentials: true })
         );
     }
 
+    /**
+     * Activates a user account using the provided UID and token.
+     *
+     * @param uidb64 - The base64 encoded user ID.
+     * @param token - The activation token.
+     * @returns An Observable of the HTTP GET request.
+     */
     public activateAccount(uidb64: string, token: string) {
         const url = `${environment.apiUrl}/activate/${uidb64}/${token}/`;
         return this.http.get(url);
     }
 
+    /**
+     * Sends a request to reset the password for the given email address.
+     *
+     * @param email - The email address for which to request a password reset.
+     * @returns An Observable of the HTTP response from the password reset request.
+     */
     public requestPasswordReset(email: string) {
         const url = `${environment.apiUrl}/password-reset/`;
         const body = { email: email };
         return this.http.post(url, body);
     }
 
+    /**
+     * Resets the user's password using the provided uidb64 and token.
+     *
+     * @param uidb64 - The base64 encoded user ID.
+     * @param token - The password reset token.
+     * @param password - The new password to set.
+     * @returns An Observable of the HTTP response.
+     */
     public resetPassword(uidb64: string, token: string, password: string) {
         const url = `${environment.apiUrl}/password-reset-confirm/${uidb64}/${token}/`;
         const body = { password: password };
         return this.http.post(url, body);
     }
 
-    // Neue Methode zur Speicherung des Tokens
+    /**
+     * Stores the authentication token in either localStorage or sessionStorage based on the rememberMe flag.
+     *
+     * @param token - The authentication token to be stored.
+     * @param rememberMe - A boolean flag indicating whether to store the token in localStorage (true) or sessionStorage (false).
+     */
     public storeToken(token: string, rememberMe: boolean): void {
         if (rememberMe) {
             localStorage.setItem(this.TOKEN_KEY, token);
@@ -72,7 +112,11 @@ export class AuthService {
         }
     }
 
-    // Neue Methode zur Abfrage des Tokens
+    /**
+     * Retrieves the authentication token from localStorage or sessionStorage.
+     *
+     * @returns The authentication token if it exists, otherwise null.
+     */
     public getToken(): string | null {
         return (
             localStorage.getItem(this.TOKEN_KEY) ||
